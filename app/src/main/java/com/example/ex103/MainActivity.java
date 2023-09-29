df989d310003db69d1df9448456f7049423d43da
orira22 <oriroitzaid@gmail.com>
2023-09-29 15:53:15 +0300

    Finished the design of the main activity

--- app/src/main/java/com/example/ex103/MainActivity.java
+++ app/src/main/java/com/example/ex103/MainActivity.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author Ori Roitzaid <or1901 @ bs.amalnet.k12.il>
+ * @version	1
+ * @since 29/9/2023
+ * The main activity:
+ * Collects data of a mathematical series in an alert dialog and displays the 20 first values
+ * of the series in a list view. Also shows the data of each series value when it's clicked.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
